5a8ca77b53fdc1a020e1e67bcd4468a21a009721
cesque <euqsec@gmail.com>
2017-01-22 01:58:11 +0000

    add basic Lexicon class

--- LinguisticsApp1/Grammar/Lexeme.cs
+++ LinguisticsApp1/Grammar/Lexeme.cs
@@ -10,8 +10,15 @@ namespace yod.Grammar
 {
     public class Lexeme
     {
-        public Phonology.Word Translated;
+        public Phonology.Word Lemma;
         public string English;
         public PartOfSpeech POS;
+
+        public Lexeme(string english, Phonology.Word lemma, PartOfSpeech pos) 
+        {
+            English = english;
+            Lemma = lemma;
+            POS = pos;
+        }
     }
 }
---
+++ LinguisticsApp1/Grammar/Lexicon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yod.Grammar
+{
+    class Lexicon : IEnumerable<Lexeme>
+    {
+        private List<Lexeme> Lexemes;
+
+        public Lexicon()
+        {
+            Lexemes = new List<Lexeme>();
+        }
+
+        public Lexeme this[string englishLemma]
+        {
+            get
+            {
+                return Lexemes.Find(x => x.English == englishLemma);
+            }
+        }
+
+        public void Add(Lexeme lexeme)
+        {
+            Lexemes.Add(lexeme);
+        }
+
+        public void Add(string english, Phonology.Word lemma, PartOfSpeech pos)
+        {
+            this.Add(new Lexeme(english, lemma, pos));
+        }
+
+        public bool Contains(string english)
+        {
+            return Lexemes.Exists(x => x.English == english);
+        }
+
+        public IEnumerator<Lexeme> GetEnumerator()
+        {
+            return Lexemes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this.GetEnumerator();
+        }
+    }
+}
